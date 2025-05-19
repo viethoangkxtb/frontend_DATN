08175291a15e2131c0ad82c195398d80d7bca827
viethoangkxtb <viethoangkxtb@gmail.com>
2025-05-20 00:00:18 +0700

    PageSize 5 to 6

--- src/components/client/card/job.card.tsx
+++ src/components/client/card/job.card.tsx
@@ -25,7 +25,7 @@ const JobCard = (props: IProps) => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const [current, setCurrent] = useState(1);
-    const [pageSize, setPageSize] = useState(5);
+    const [pageSize, setPageSize] = useState(6);
     const [total, setTotal] = useState(0);
     const [filter, setFilter] = useState("");
     const [sortQuery, setSortQuery] = useState("sort=-updatedAt");
